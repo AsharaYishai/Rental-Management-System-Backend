54885d277729c299acb6e60104b098e47f9803da
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-26 13:43:57 +0530

    Hardware item dto

---
+++ src/main/java/org/rms/dto/HardwareItem.java
@@ -0,0 +1,20 @@
+package org.rms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class HardwareItem {
+    private Long itemId;
+    private String name;
+    private String availability;
+    private Date finePerDay;
+    private Date rentalPerDay;
+}
